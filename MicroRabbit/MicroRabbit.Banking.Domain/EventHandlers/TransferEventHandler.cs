76113ca78f7694d9bb17808af824a6382352dc88
Mohammad Bafkar <bafkar.mohammad@gmail.com>
2020-08-30 01:39:48 +0430

    Initial Commit of Transfer Microservice Projects

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/EventHandlers/TransferEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus;
+
+namespace MicroRabbit.Banking.Domain.EventHandlers
+{
+    public class TransferEventHandler : IEventHandler<TransferCreatedEvent>
+    {
+        public Task Handle(TransferCreatedEvent @event)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
