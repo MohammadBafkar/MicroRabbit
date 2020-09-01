009c5444b038a50e1ab3e07f68fc9b09666a8881
Mohammad Bafkar <bafkar.mohammad@gmail.com>
2020-09-01 23:55:41 +0430

    Complete Transfer Microservice

--- MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Startup.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.EventHandlers;
+using MicroRabbit.Transfer.Domain.Events;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MicroRabbit.Transfer.Api
 {
@@ -25,7 +33,22 @@ namespace MicroRabbit.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+            });
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("V1", new OpenApiInfo { Title = "Transfer Microservice", Version = "V1" });
+            });
+            services.AddMediatR(GetType());
             services.AddControllers();
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.Register(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +58,9 @@ namespace MicroRabbit.Transfer.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/V1/swagger.json", "Transfer Microservice V1"); });
 
             app.UseHttpsRedirection();
 
@@ -46,6 +72,14 @@ namespace MicroRabbit.Transfer.Api
             {
                 endpoints.MapControllers();
             });
+
+            ConfigureEventBus(app);
+        }
+
+        private void ConfigureEventBus(IApplicationBuilder app)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            eventBus.Subscribe<TransferCreatedEvent, TransferEventHandler>();
         }
     }
 }
---
+++ MicroRabbit/MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Application.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly ITransferRepository _transferRepository;
+
+        public TransferService(ITransferRepository transferRepository)
+        {
+            _transferRepository = transferRepository;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _transferRepository.GetTransferLogs();
+        }
+    }
+}
